9b0b978bf30a67940a6f6af43de2572cb8788c14
Dawid Deszcz <d.deszcz@gmail.com>
2023-02-21 12:41:43 +0100

    Point 3. Top 10 Worst selling products

---
+++ task-3.sql
@@ -0,0 +1,7 @@
+SELECT products.product_name, TO_CHAR(SUM(order_details.quantity * order_details.unit_price * (1 - order_details.discount)), '9999999999') AS sum
+FROM order_details
+    INNER JOIN products
+ON order_details.product_id = products.product_id
+GROUP BY products.product_name
+ORDER BY sum
+    LIMIT 10;
